b0a2d6b69ea5f337f52156dfb478c3850555c53f
Paweł Dowbusz <paweldowbusz@MacBook-Air-Pawe.local>
2024-12-02 17:52:52 +0100

    Updated header on the home page

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ winner_address = None  # Adres zwycięzcy
 def home():
     return '''
         <h1>Congrats!</h1>
-        <p>Use the form below to claim the reward.</p>
+        <p>(Use Solana wallet below) Use the form below to claim the reward.</p>
         <form action="/claim" method="post">
             <input type="text" name="address" placeholder="Enter your wallet address" required>
             <button type="submit">Submit</button>
